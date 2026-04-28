e1af63433ce9f342b72a902837cc069aa0f1f2fd
Will_Langley <langley1809@gmail.com>
2026-04-28 11:37:58 -0700

    Admin Landing page and new empty view for settings created

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,10 +34,14 @@ android {
     }
     buildFeatures {
         compose = true
+        viewBinding = true;
     }
 }
 
 dependencies {
+    implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -46,6 +50,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/team7/database/entities/User.java
+++ app/src/main/java/com/example/team7/database/entities/User.java
@@ -2,6 +2,7 @@ package com.example.team7.database.entities;
 
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
+import java.util.Objects;
 
 @Entity(tableName = "users")
 public class User {
@@ -10,8 +11,58 @@ public class User {
     private String username;
     private String userPassword;
 
+    private boolean isAdmin;
+
     public User(String username, String password) {
         this.username = username;
         this.userPassword = password;
+        isAdmin = false;
+    }
+
+
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return userId == user.userId && Objects.equals(username, user.username) && Objects.equals(userPassword, user.userPassword);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, username, userPassword);
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
     }
 }
