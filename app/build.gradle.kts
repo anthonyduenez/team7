47be0bbc9f7d3650441758fa49b81cfc0714fed9
Will_Langley <langley1809@gmail.com>
2026-05-05 10:54:55 -0700

    android manifest update for add clothes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.activity)
+    implementation(libs.androidx.activity.ktx)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
