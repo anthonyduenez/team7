1ff4e4c873573ff40ada9d4ea442953ac6321a2f
Will_Langley <langley1809@gmail.com>
2026-05-08 22:04:03 -0700

    Add and Remove Users is complete on Admin Page

--- app/src/main/java/com/example/team7/AdminClasses/AddRemoveUsers.java
+++ app/src/main/java/com/example/team7/AdminClasses/AddRemoveUsers.java
@@ -1,26 +1,78 @@
 package com.example.team7.AdminClasses;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import com.example.team7.UserAdapter;
+import com.example.team7.database.WardrobeRepository;
+import com.example.team7.database.entities.User;
+import com.example.team7.databinding.ActivityAddRemoveUsersBinding;
+import com.example.team7.databinding.ActivityAdminSettingsBinding;
 
-import com.example.team7.R;
+import java.util.ArrayList;
 
 public class AddRemoveUsers extends AppCompatActivity {
+
+    private ActivityAddRemoveUsersBinding binding;
+    private WardrobeRepository repository;
+    private UserAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_add_remove_users);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityAddRemoveUsersBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        repository = WardrobeRepository.getRepository(getApplication());
+        adapter = new UserAdapter(repository);
+        binding.userRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        binding.userRecyclerView.setAdapter(adapter);
+        repository.getAllUsers().observe(this, users -> {
+            if(users != null)
+                adapter.setUsers(users);
         });
+
+        binding.createUserButton.setOnClickListener(v -> showCreateUserDialogue());
+        binding.back.setOnClickListener(v -> finish());
+
+    }
+
+    public void showCreateUserDialogue() {
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        layout.setPadding(40,20,40,0);
+
+        EditText usernameInput = new EditText(this);
+        usernameInput.setHint("Username");
+        EditText passwordInput = new EditText(this);
+        passwordInput.setHint("Password");
+
+        layout.addView(usernameInput);
+        layout.addView(passwordInput);
+
+        new AlertDialog.Builder(this)
+                .setTitle("Create New User")
+                .setView(layout)
+                .setPositiveButton("Create", ((dialog, which) -> {
+                    String username = usernameInput.getText().toString().trim();
+                    String password = passwordInput.getText().toString().trim();
+                    if(username.isEmpty() || password.isEmpty()) {
+                        Toast.makeText(this, "Username and Password Required", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
+                    User newUser = new User(username, password);
+                    repository.insertUser(newUser);
+
+                    Toast.makeText(this, "User Created!", Toast.LENGTH_SHORT).show();
+                }))
+                .setNegativeButton("Cancel", null)
+                .show();
+
     }
 }
--- app/src/main/java/com/example/team7/database/dao/UserDao.java
+++ app/src/main/java/com/example/team7/database/dao/UserDao.java
@@ -2,22 +2,32 @@ package com.example.team7.database.dao;
 
 import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
+import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
 import androidx.room.Transaction;
+import androidx.room.Update;
 
 import com.example.team7.database.entities.User;
 import com.example.team7.database.relations.UserWithOutfits;
+import java.util.List;
 
 @Dao
 public interface UserDao {
     @Insert
-    long insertUser(User user);
+    void insertUser(User user);
 
     @Query("SELECT * FROM users WHERE userId = :userId")
     User getUserById(int userId);
 
+    @Query("SELECT * FROM users ORDER BY username ASC")
+    LiveData<List<User>> getAllUsers();
 
+    @Delete
+    void delete(User user);
+
+    @Update
+    void update(User user);
 
     @Transaction
     @Query("SELECT * FROM users WHERE userId = :userId")
